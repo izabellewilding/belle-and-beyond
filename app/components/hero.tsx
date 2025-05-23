972644d882f1c8eea76a0152237a6529d50c7780
Izabelle Wilding <izabellewilding@gmail.com>
2025-05-23 17:45:15 +0400

    Remove profile card & fix apostrophe errors

--- app/components/hero.tsx
+++ app/components/hero.tsx
@@ -1,7 +1,6 @@
 // pages/index.tsx
 import Image from "next/image";
 import { Button } from "./button";
-import { ProfileCard } from "./profile-card";
 
 export const Hero = () => {
   return (
@@ -22,19 +21,17 @@ export const Hero = () => {
             Discover the extraordinary and find beauty in your travels
             <br />
           </h2>
-          {/* <div className="mb-6">
-            <ProfileCard />
-          </div> */}
+
           <p className="text-gray-600 mb-6 text-base leading-relaxed">
-            Hey! I'm Izabelle, a photographer, web developer and traveller based
-            in Tbilisi, Georgia. I'm drawn to the quiet beauty of nature, little
-            surprises, and the raw, authentic feel of old, rustic places. I
-            believe travel isn’t just about ticking off destinations—it’s about
-            finding those unexpected, peaceful moments that stay with you. I
-            love exploring popular places differently:{" "}
-            <strong> finding stillness in the noise</strong>, charm in the old
-            and rustic, and magic in the unplanned. Here, you’ll find practical
-            travel tips and guides to help you do the same.
+            Hey! I&apos;m Izabelle, a photographer, web developer and traveller
+            based in Tbilisi, Georgia. I&apos;m drawn to the quiet beauty of
+            nature, little surprises, and the raw, authentic feel of old, rustic
+            places. I believe travel isn&apos;t just about ticking off
+            destinations—it&apos;s about finding those unexpected, peaceful
+            moments that stay with you. I love exploring popular places
+            differently: <strong> finding stillness in the noise</strong>, charm
+            in the old and rustic, and magic in the unplanned. Here, you&apos;ll
+            find practical travel tips and guides to help you do the same.
           </p>
           <div className="flex gap-4">
             <Button text="Visit Blog" href="/blog" />
