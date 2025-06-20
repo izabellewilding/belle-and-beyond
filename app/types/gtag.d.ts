d85c84ce5e6d81592118f31027df0126e4b1229f
Izabelle Wilding <izabellewilding@gmail.com>
2025-06-20 12:00:12 +0100

    fix build errors

--- app/types/gtag.d.ts
+++ app/types/gtag.d.ts
@@ -1,10 +1,15 @@
 declare global {
   interface Window {
-    dataLayer: any[];
+    dataLayer: unknown[];
     gtag: (
       command: 'config' | 'event' | 'js' | 'set',
       targetId: string,
-      config?: Record<string, any>
+      config?: Record<string, unknown> | {
+        affiliate_id: string;
+        url: string;
+        timestamp: string;
+        [key: string]: unknown;
+      }
     ) => void;
   }
 }
