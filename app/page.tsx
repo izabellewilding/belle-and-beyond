04639a1f7d39ec5504897a9a758f1d55da38f462
Izabelle Wilding <izabellewilding@gmail.com>
2025-11-12 21:07:39 +0000

    Add alternative latest articles

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,23 @@
 import { Hero } from "./components/hero";
 import { Navigation } from "./components/navigation";
 import { RecentPosts } from "./components/recent-posts";
+import { RecentPostsAlternative } from "./components/recent-posts-alternative";
 import { Footer } from "./components/footer";
 import { About } from "./components/about";
 import { GalleryDirectory } from "./components/GalleryDirectory";
 import { Contact } from "./components/contact";
 
 export default function Home() {
+  // Set to true to use the alternative layout, false for the original
+  const useAlternativeLayout = false;
+
   return (
     <div className="">
       <Navigation />
       <Hero />
       {/* Gallery below hero, aligned to the right side */}
 
-      <RecentPosts />
+      {useAlternativeLayout ? <RecentPostsAlternative /> : <RecentPosts />}
       <div className="w-full flex justify-end">
         <GalleryDirectory />
       </div>
