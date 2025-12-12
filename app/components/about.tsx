3d9101634a0c0a499c480bf1f8fbce2e4e4d67eb
Izabelle Wilding <izabellewilding@gmail.com>
2025-12-12 15:49:59 +0000

    about section spacing

--- app/components/about.tsx
+++ app/components/about.tsx
@@ -19,35 +19,35 @@ export const About = () => {
     <section
       ref={sectionRef}
       id="our-story"
-      className="relative min-h-screen w-full flex flex-col lg:flex-row m-0 p-0 bg-[#ffeced] overflow-x-hidden overflow-y-visible z-30"
+      className="relative min-h-screen w-full flex flex-col xl:flex-row m-0 p-0 bg-[#ffeced] overflow-x-hidden overflow-y-visible z-30"
     >
       {/* Left Section - Tilted Photo */}
-      <div className="relative w-full lg:w-[45%] h-[50vh] lg:h-screen flex items-center justify-center lg:justify-end px-6 pt-6 pb-6 lg:px-0 lg:pt-0 lg:pb-0 lg:pr-4 overflow-visible z-20">
+      <div className="relative w-full xl:w-[45%] h-[50vh] xl:h-screen flex items-center justify-center xl:justify-end px-6 pt-6 pb-6 xl:px-0 xl:pt-0 xl:pb-0 xl:pr-10 overflow-visible z-20">
         <motion.div
           initial={{ opacity: 0, x: -50 }}
           whileInView={{ opacity: 1, x: 0 }}
           viewport={{ once: true }}
           transition={{ duration: 0.8, ease: "easeOut" }}
-          className="relative w-full max-w-sm aspect-square lg:max-w-lg lg:aspect-auto lg:h-[calc(85%-100px)]"
+          className="relative w-full max-w-sm aspect-square xl:max-w-lg xl:aspect-auto xl:h-[calc(85%-100px)]"
         >
           {/* Dark background layer */}
-          <div className="absolute inset-0 bg-[#172531] rotate-2 lg:rotate-3 transform origin-center" />
+          <div className="absolute inset-0 bg-[#172531] rotate-2 xl:rotate-3 transform origin-center" />
 
           {/* Image */}
-          <div className="relative w-full h-full p-3 lg:p-4 -rotate-2 lg:-rotate-3 transform origin-center">
+          <div className="relative w-full h-full p-3 xl:p-4 -rotate-2 xl:-rotate-3 transform origin-center">
             <Image
               src="/images/izzy.jpeg"
               alt="Izabelle and Zia, digital nomads"
               fill
               className="object-cover"
-              sizes="(min-width: 1024px) 45vw, 100vw"
+              sizes="(min-width: 1280px) 45vw, 100vw"
             />
           </div>
         </motion.div>
       </div>
 
       {/* Right Section - Text Content */}
-      <div className="relative w-full lg:w-[55%] h-auto lg:h-screen flex items-center justify-center bg-[#ffeced] px-8 md:px-10 lg:pl-4 lg:-ml-8 lg:pr-20 xl:pl-8 xl:-ml-12 xl:pr-24 lg:py-0 pb-20 lg:pb-0">
+      <div className="relative w-full max-w-[700px] xl:max-w-none xl:w-[55%] h-auto xl:h-screen flex items-center justify-center bg-[#ffeced] px-8 md:px-10 xl:pl-10 xl:-ml-6 xl:pr-24 xl:py-0 pb-20 xl:pb-0 xl:mx-0 mx-auto">
         <motion.div
           initial={{ opacity: 0, x: 50 }}
           whileInView={{ opacity: 1, x: 0 }}
